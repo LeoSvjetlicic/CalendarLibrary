a3f346fb089d827a7167ebf33591542601e41ccf
LeoSvjetlicic <leo.svjetlicic@gmail.com>
2024-09-03 14:57:11 +0200

    Add setup for publishing

--- calendarLibrary/build.gradle.kts
+++ calendarLibrary/build.gradle.kts
@@ -1,12 +1,17 @@
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.jetbrains.kotlin.android)
+    id("maven-publish")
 }
 
 android {
     namespace = "com.example.calendarlibrary"
     compileSdk = 34
-
+    publishing {
+        singleVariant("release") {
+            withSourcesJar()
+        }
+    }
     defaultConfig {
         minSdk = 28
 
@@ -16,7 +21,7 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
@@ -37,7 +42,6 @@ android {
         jvmTarget = "1.8"
     }
 }
-
 dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx.v283)
@@ -53,4 +57,24 @@ dependencies {
     debugImplementation(libs.ui.test.manifest)
     implementation(libs.androidx.activity.compose)
     implementation(libs.androidx.navigation.compose)
+}
+publishing {
+    publications {
+        create<MavenPublication>("release") {
+            groupId = project.findProperty("gpr.groupid") as String? ?: ""
+            artifactId = "calendarlibrary"
+            version = project.findProperty("gpr.version") as String? ?: ""
+
+        }
+    }
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/LeoSvjetlicic/CalendarLibrary")
+            credentials {
+                username = project.findProperty("gpr.user") as String? ?: ""
+                password = project.findProperty("gpr.token") as String? ?: ""
+            }
+        }
+    }
 }
